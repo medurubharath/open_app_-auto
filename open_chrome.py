da78f8e142b506babd540ea362f5314aa024dd55
M bharath <60010887+medurubharath@users.noreply.github.com>
2020-06-13 13:11:08 +0530

    python file

---
+++ open_chrome.py
@@ -0,0 +1,7 @@
+import webbrowser
+web="https://www.google.com/"
+web1='https://www.youtube.com/'
+web2="https://fast.com/"
+webbrowser.open_new_tab(web)
+webbrowser.open_new_tab(web1)
+webbrowser.open_new_tab(web2)
